29d6a701bd89cc1995d709c226b6a5512915ae73
manuelsaezsa <manuelsaezsa@gmail.com>
2024-03-12 20:44:06 -0300

    proyecto terminado con comentarios

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-    $('a[href^="#"]').on('click', function(event) {
-        var target = $(this.getAttribute('href'));
-        if( target.length ) {
-            event.preventDefault();
+$(document).ready(function() {                             // Selecciona todos los enlaces que empiezan con #
+    $('a[href^="#"]').on('click', function(event) {       // Obtiene el elemento al que apunta el enlace
+        var target = $(this.getAttribute('href'));        // Verifica si el elemento existe en el documento
+        if( target.length ) {                             // Evita el comportamiento por defecto del enlace
+            event.preventDefault();                       // Realiza una animación de desplazamiento suave hasta el elemento
             $('html, body').stop().animate({
                 scrollTop: target.offset().top
             }, 1000);
